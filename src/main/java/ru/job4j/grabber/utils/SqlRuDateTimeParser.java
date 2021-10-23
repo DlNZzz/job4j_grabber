d89dce4bef08ce9d216ca0a8be900a68eb54fa31
Dinz <kostan-82@yandex.ru>
2021-10-23 12:32:39 +0300

    2.1. Преобразование даты [#289476]

---
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -0,0 +1,46 @@
+package ru.job4j.grabber.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Map;
+
+public class SqlRuDateTimeParser implements DateTimeParser {
+
+    private static final Map<String, String> MONTHS = Map.ofEntries(
+            Map.entry("янв", "января"),
+            Map.entry("фев", "февраля"),
+            Map.entry("мар", "марта"),
+            Map.entry("апр", "апреля"),
+            Map.entry("май", "мая"),
+            Map.entry("июн", "июня"),
+            Map.entry("июл", "июля"),
+            Map.entry("авг", "августа"),
+            Map.entry("сен", "сентября"),
+            Map.entry("окт", "октября"),
+            Map.entry("ноя", "ноября"),
+            Map.entry("дек", "декабря"),
+            Map.entry("сегодня", java.time.LocalDateTime.now().getMonth().getValue() + ""),
+            Map.entry("вчера",
+                    java.time.LocalDateTime.now().minusDays(1).getMonth().getValue() + "")
+            );
+
+    @Override
+    public LocalDateTime parse(String parse) {
+        String[] parseArray = parse.split(" ");
+        parseArray[1] = MONTHS.getOrDefault(parseArray[1], parseArray[1]);
+        parse = Arrays.stream(parseArray).reduce((x, y) -> x + " " + y).get();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yy, HH:mm");
+        try {
+            if (Integer.parseInt(parseArray[1]) >= 0) {
+                formatter = DateTimeFormatter.ofPattern("dd M yy, HH:mm");
+            }
+        } catch (NumberFormatException ignored) { }
+        return LocalDateTime.parse(parse, formatter);
+    }
+
+    public static void main(String[] args) {
+        SqlRuDateTimeParser sqlRuDateTimeParser = new SqlRuDateTimeParser();
+        System.out.println(sqlRuDateTimeParser.parse("12 вчера 21, 12:19"));
+    }
+}

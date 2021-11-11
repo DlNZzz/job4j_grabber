31f92febb2863a1cb7aebf2961d120085019830b
Dinz <kostan-82@yandex.ru>
2021-11-11 20:55:40 +0300

    1. Реализация кеша на SoftReference [#1592 #226280]

--- src/main/java/ru/job4j/cache/DirFileCache.java
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -2,6 +2,7 @@ package ru.job4j.cache;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class DirFileCache extends AbstractCache<String, String> {
 
@@ -15,7 +16,7 @@ public class DirFileCache extends AbstractCache<String, String> {
     protected String load(String key) {
         String text = "";
         try {
-            text = Files.readString(new File(cachingDir + "\\" + key).toPath());
+            text = Files.readString(Path.of(cachingDir, key));
         } catch (IOException e) {
             e.printStackTrace();
         }

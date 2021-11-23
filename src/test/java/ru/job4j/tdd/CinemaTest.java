f558a983229ebd93e2428916aa794c9a59d03569
Dinz <kostan-82@yandex.ru>
2021-11-23 14:19:51 +0300

    2. Что такое TDD? [#4918]

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -3,6 +3,7 @@ package ru.job4j.tdd;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -11,16 +12,18 @@ import java.util.List;
 
 public class CinemaTest {
 
+    @Ignore
     @Test
     public void buy() {
         Account account = new AccountCinema();
         Cinema cinema = new Cinema3D();
         Calendar date = Calendar.getInstance();
-        date.set(2020, 10, 10, 23, 00);
+        date.set(2021, 11, 28, 23, 00);
         Ticket ticket = cinema.buy(account, 1, 1, date);
         assertThat(ticket, is(new Ticket3D()));
     }
 
+    @Ignore
     @Test
     public void find() {
         Cinema cinema = new Cinema3D();
@@ -28,4 +31,33 @@ public class CinemaTest {
         List<Session> sessions = cinema.find(session -> true);
         assertThat(sessions, is(Arrays.asList(new Session3D())));
     }
+
+    @Ignore
+    @Test(expected = NullPointerException.class)
+    public void buyTwo() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, 11, 23, 23, 00);
+        cinema.buy(account, 1, 1, date);
+        cinema.buy(account, 1, 1, date);
+    }
+
+    @Ignore
+    @Test(expected = NullPointerException.class)
+    public void date() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(201, 11, 23, 23, 00);
+        cinema.buy(account, 1, 1, date);
+    }
 }
+
+
+
+
+
+
+
+
---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,11 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+public class GeneratorTest {
+
+    @Test
+    public void produce() {
+
+    }
+}

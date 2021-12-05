afa4a06a750b3a5ea6c95ed20f8eac67f2be45f1
Dinz <kostan-82@yandex.ru>
2021-12-05 19:24:41 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851 #235050]

--- src/test/java/ru/job4j/design/srp/ReportJSONTest.java
+++ src/test/java/ru/job4j/design/srp/ReportJSONTest.java
@@ -1,5 +1,37 @@
 package ru.job4j.design.srp;
 
+import org.junit.Test;
+
+import javax.xml.bind.JAXBException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class ReportJSONTest  {
 
+    @Test
+    public void generate() throws JAXBException {
+        MemStore store = new MemStore();
+        Calendar date = new GregorianCalendar(2020, Calendar.JANUARY, 5);
+        Employee employee = new Employee("A", date, date, 100);
+        store.add(employee);
+        Report reportJSON = new ReportJSON(store);
+        StringBuilder expected = new StringBuilder()
+                .append("[{")
+                .append("\"name\":")
+                .append("\"A\",")
+                .append("\"hired\":")
+                .append("{\"year\":2020,\"month\":0,\"dayOfMonth\":5,"
+                        + "\"hourOfDay\":0,\"minute\":0,\"second\":0},")
+                .append("\"fired\":")
+                .append("{\"year\":2020,\"month\":0,\"dayOfMonth\":5,"
+                        + "\"hourOfDay\":0,\"minute\":0,\"second\":0},")
+                .append("\"salary\":")
+                .append("100.0")
+                .append("}]");
+        assertThat(reportJSON.generate(filter -> true), is(expected.toString()));
+    }
 }
--- src/test/java/ru/job4j/design/srp/ReportXMLTest.java
+++ src/test/java/ru/job4j/design/srp/ReportXMLTest.java
@@ -2,18 +2,23 @@ package ru.job4j.design.srp;
 
 import org.junit.Test;
 
+import javax.xml.bind.JAXBException;
 import java.util.Calendar;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class ReportXMLTest {
 
     @Test
-    public void generate() {
-        MemStore memStore = new MemStore();
+    public void generate() throws JAXBException {
+        MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
         Employee employee = new Employee("A", now, now, 100);
-        Employee employee2 = new Employee("B", now, now, 200);
-        memStore.add(employee);
-        memStore.add(employee2);
-        ReportXML reportXML = new ReportXML(memStore);
+        store.add(employee);
+        Report reportXML = new ReportXML(store);
+        StringBuilder expected = new StringBuilder()
+                .append("");
+        assertThat(reportXML.generate(filter -> true), is(expected.toString()));
     }
 }

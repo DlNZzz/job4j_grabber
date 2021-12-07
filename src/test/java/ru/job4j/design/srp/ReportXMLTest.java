641368004a9175bac4b70cfb390d5e2c1a752834
Dinz <kostan-82@yandex.ru>
2021-12-07 09:55:32 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851 #235050]

--- src/test/java/ru/job4j/design/srp/ReportXMLTest.java
+++ src/test/java/ru/job4j/design/srp/ReportXMLTest.java
@@ -4,7 +4,10 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.xml.bind.JAXBException;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 import static org.hamcrest.Matchers.is;
@@ -15,7 +18,9 @@ public class ReportXMLTest {
     @Test
     public void generate() throws JAXBException {
         MemStore store = new MemStore();
+        ZonedDateTime currentDate = ZonedDateTime.now( ZoneOffset.UTC );
         Calendar date = new GregorianCalendar(2020, Calendar.JANUARY, 5);
+        date.add(Calendar.MILLISECOND, -date.getTimeZone().getOffset(date.getTimeInMillis()));
         Employee employee = new Employee("A", date, date, 100);
         store.add(employee);
         Report reportXML = new ReportXML(store);
@@ -23,8 +28,8 @@ public class ReportXMLTest {
                 .append("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>")
                 .append("\n<employees>")
                 .append("\n    <employees>")
-                .append("\n        <fired>2020-01-05T00:00:00+03:00</fired>")
-                .append("\n        <hired>2020-01-05T00:00:00+03:00</hired>")
+                .append("\n        <fired>2020-01-05T00:00:00Z</fired>")
+                .append("\n        <hired>2020-01-05T00:00:00Z</hired>")
                 .append("\n        <name>A</name>")
                 .append("\n        <salary>100.0</salary>")
                 .append("\n    </employees>")

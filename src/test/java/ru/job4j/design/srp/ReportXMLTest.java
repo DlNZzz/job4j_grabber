bd33c8dac6acee02d5937552e11adbe09c1c2bc8
Dinz <kostan-82@yandex.ru>
2021-12-07 17:51:06 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851 #235050]

--- src/test/java/ru/job4j/design/srp/ReportXMLTest.java
+++ src/test/java/ru/job4j/design/srp/ReportXMLTest.java
@@ -18,7 +18,7 @@ public class ReportXMLTest {
     @Test
     public void generate() throws JAXBException {
         MemStore store = new MemStore();
-        ZonedDateTime currentDate = ZonedDateTime.now( ZoneOffset.UTC );
+        ZonedDateTime currentDate = ZonedDateTime.now(ZoneOffset.UTC);
         Calendar date = new GregorianCalendar(2020, Calendar.JANUARY, 5);
         date.add(Calendar.MILLISECOND, -date.getTimeZone().getOffset(date.getTimeInMillis()));
         Employee employee = new Employee("A", date, date, 100);
